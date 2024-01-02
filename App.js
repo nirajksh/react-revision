d2492203e0d52fb6ce6441909bd1e40a72b1ec22
Niraj Kumar Singh <91699259+nirajksh@users.noreply.github.com>
2024-01-02 12:14:53 +0530

    Assignmen 1

---
+++ App.js
@@ -0,0 +1,22 @@
+
+const head = React.createElement("h1",{
+    id:"title",
+
+}
+,"heading 1")
+
+const head2 = React.createElement("h2",{
+    id:"title",
+},"heading 2")
+
+const container = React.createElement("div",{
+    id:"container",
+
+},"container")
+
+const container2 = React.createElement("div",{
+    id:"title"
+},[head,container,head2])
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(container2)
